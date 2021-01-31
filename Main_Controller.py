352f5785da9b193a325fbc64f96df679e2cac7b2
Brandon Wingard <56277974+AegisSSC@users.noreply.github.com>
2021-01-31 06:01:22 -0500

    last commit 6:01

--- Main_Controller.py
+++ Main_Controller.py
@@ -11,14 +11,14 @@ from usercomparetospotify import *
 # print(X)
 # def generate__reccomended_playlist():
     # if not Read_from_File("output.txt"):
-s = spotify_featured_playlists()
-s.main_component()
+# s = spotify_featured_playlists()
+# s.main_component()
 #s = spotify_featured_playlists()
 #s.main_component()
 UserComparisonTool = UserComparisonTool()
 Spotify_Playlist = Read_from_File("output.txt")
-# username = input("Enter your username: ")
-user = user_playlist()
+username = input("Enter your username: ")
+user = user_playlist(username)
 user.main_component()
 recommendation = UserComparisonTool.usercomparison(user.Database_Information, Spotify_Playlist)
 print('\n' + recommendation+ '\n\n')
