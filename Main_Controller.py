816eb3f7c694ada28319485ae59c8f19fab9e6ae
Aobasi <aobasi4@gmail.com>
2021-01-31 01:26:23 -0500

    OMG we did it

--- Main_Controller.py
+++ Main_Controller.py
@@ -1,8 +1,10 @@
 from getuserplaylists import *
 from getspotifyplaylists import *
+from FileHandler import * 
 
-
+S = Read_from_File('output.txt')
+print(S)
 # s = spotify_featured_playlists()
 # s.main_component()
-u = user_playlist()
-u.main_component()
+#u = user_playlist()
+#u.main_component()
